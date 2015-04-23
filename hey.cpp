8dec66932d0be630c1774fd7e9d90321f80f0750
Brian Kelley <bkelley620@gmail.com>
2015-04-22 21:07:40 -0400

    Preserved the Linux community attitude

--- hey.cpp
+++ hey.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 void print_help(const char* name) {
 	std::cout << "Usage: " << std::endl;
@@ -16,6 +17,7 @@ int main(int argc, const char* argv[]) {
 	} else {
 		if (std::string(argv[1]) != std::string("tux,")) {
 			// They don't even know who they're asking. Let's teach them.
+			std::cout << "Learn how to Linux, skrub" << std::endl;
 			std::string query = std::string("xdg-open http://lmgtfy.com/?q=what's+the+name+of+linux's+mascot? > /dev/null");
 			popen(query.c_str(), "r");
 			print_help(argv[0]);
@@ -24,15 +26,20 @@ int main(int argc, const char* argv[]) {
 	}
 
 	std::string query;
+	std::string sentence;
 
 	for (int i = 2; i < argc; ++i) {
 		// Create the end part of the url, separate words by '+'
 		if (i != 2) {
 			query += '+';
+			sentence += ' ';
 		}
 		query += argv[i];
+		sentence += argv[i];
 	}
 
+	std::cout << "Seriously? \"" << sentence << "\"?" << " *sigh*. I guess I can help with that." << std::endl;
+	sleep(1);
 	// Build and run query
 	query = std::string("xdg-open http://lmgtfy.com/?q=") + query;
 	query += std::string(" > /dev/null");
